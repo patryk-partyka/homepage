7c25f0f7f82db33d9a5a0e7c67c58ccb78081933
Patryk Partyka <patrykpartyka@wp.pl>
2020-05-04 21:47:36 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Patryk';
+const age = '22';
+
+
+alert('Witaj na mojej stronie!');
+
+console.log('Witaj na mojej stronie!')
+console.log(`Nazywam się ${firstName} i mam ${age} lata.`);
