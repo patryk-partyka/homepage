d035fa39219b2c37a674b7d363824a6a7da60f57
Patryk Partyka <patrykpartyka@wp.pl>
2020-05-13 13:51:23 +0200

    html,css,js changes

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const firstName = 'Patryk';
-const age = '22';
+const firstName = "Patryk";
+const age = "22";
 
+alert("Witaj na mojej stronie!");
 
-alert('Witaj na mojej stronie!');
-
-console.log('Witaj na mojej stronie!')
+console.log("Witaj na mojej stronie!");
 console.log(`Nazywam się ${firstName} i mam ${age} lata.`);
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Nazywam się ${firstName} i mam ${age} lata.`
