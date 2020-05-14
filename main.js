0064a4751d168ea113c6692bb0d66aa0c0a01ddb
Patryk Partyka <patrykpartyka@wp.pl>
2020-05-14 17:33:50 +0200

    BEM class(fixed)

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ alert("Witaj na mojej stronie!");
 console.log("Witaj na mojej stronie!");
 console.log(`Nazywam się ${firstName} i mam ${age} lata.`);
 
-const heading = document.querySelector('.main-heading--js');
+const heading = document.querySelector('.main__heading--js');
 
 heading.innerHTML = `Nazywam się ${firstName} i mam ${age} lata.`
+
+const summary = document.querySelector('.week-summary--js');
+
+summary.innerHTML = 'TU JEST TEKST WPISANY DO HTML POPRZEZ JS'
